7eb22ca06ef9a9ffa70128d226eedd7799dd077c
vladimir zakotnev <vladitot@gmail.com>
2024-06-17 18:43:03 +0200

    fix logger, welcome to work with any channels

--- src/Warmers/LoggerWarmer.php
+++ src/Warmers/LoggerWarmer.php
@@ -18,16 +18,13 @@ class LoggerWarmer implements WarmerInterface
     {
         $channel = ChannelFactory::createChannel(1000);
         ContextStorage::setSystemChannel('log', $channel);
-        if (config('logging.default', 'custom')=='custom') {
-            config(['logging.channels.custom'=> [
-                'driver' => 'custom',
-                'level'=>env('LOG_LEVEL', 'error'),
-                'via' => CustomLogger::class,
-            ]]);
-            if (class_exists(ValuesMaskJsonFormatter::class)) {
-                config(['logging.channels.stderr.formatter'=>env('LOG_STDERR_FORMATTER', ValuesMaskJsonFormatter::class)]);
-            }
-            config(['logging.default'=>'custom']);
+        config(['logging.channels.custom'=> [
+            'driver' => 'custom',
+            'level'=>env('LOG_LEVEL', 'error'),
+            'via' => CustomLogger::class,
+        ]]);
+        if (class_exists(ValuesMaskJsonFormatter::class)) {
+            config(['logging.channels.stderr.formatter'=>env('LOG_STDERR_FORMATTER', ValuesMaskJsonFormatter::class)]);
         }
     }
 }
